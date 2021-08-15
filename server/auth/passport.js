d39a059160b8e4e3a8d4f62306cd5f25dc2dc173
Vishnu C Prasad <connect.vishnucp@gmail.com>
2021-08-15 09:58:07 +0530

    Setup signup, login, logout and auth routes for employer
    Setup local stratagies for employer login and signup
    
    Setup separate jwt stratagies for admin and employer

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -1,67 +1,153 @@
-const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const JWTstrategy = require('passport-jwt').Strategy;
-const Admin = require('../models/admin');
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const JWTstrategy = require("passport-jwt").Strategy;
+const Admin = require("../models/admin");
+const Employer = require("../models/employer");
 
-passport.use('admin-login', new localStrategy(
+passport.use(
+  "admin-login",
+  new localStrategy(
     {
-        usernameField: 'email',
-        passwordField: 'password'
+      usernameField: "email",
+      passwordField: "password",
     },
     async (email, password, done) => {
-        try {
-            const user = await Admin.findOne({ email });
-            if (!user) {
-                return done(null, false, { message: 'User not found' });
-            }
-
-            const validate = await user.isValidPassword(password);
-
-            if (!validate) {
-                return done(null, false, { message: 'Wrong Password' });
-            }
-
-            return done(null, user, { message: 'Logged in Successfully' });
-        } catch (error) {
-            return done(error);
+      try {
+        const user = await Admin.findOne({ email });
+        if (!user) {
+          return done(null, false, { message: "User not found" });
         }
-    }
-));
 
-passport.use('admin-google-auth', new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_ADMIN_CALLBACK_URL
-}, async (accessToken, refreshToken, profile, done) => {
-    try {
+        const validate = await user.isValidPassword(password);
+
+        if (!validate) {
+          return done(null, false, { message: "Wrong Password" });
+        }
+
+        return done(null, user, { message: "Logged in Successfully" });
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+passport.use(
+  "admin-google-auth",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_ADMIN_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
         const user = await Admin.findOne({ email: profile._json.email });
         if (!user) {
-            return done(null, false, { message: 'User not found' });
+          return done(null, false, { message: "User not found" });
         }
 
         return done(null, user);
-    } catch (error) {
+      } catch (error) {
         return done(error);
+      }
     }
-}));
+  )
+);
 
-passport.use(new JWTstrategy(
+passport.use(
+  "employer-signup",
+  new localStrategy(
     {
-        secretOrKey: process.env.JWT_SECRET,
-        jwtFromRequest: (req) => {
-            let token = null;
-            if (req && req.cookies) {
-                token = req.cookies[process.env.COOKIE_KEY];
-            }
-            return token;
+      usernameField: "email",
+      passwordField: "password",
+      passReqToCallback: true,
+    },
+    async (req, email, password, done) => {
+      try {
+        const user = await Employer.create(req.body);
+
+        return done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
+passport.use(
+  "employer-login",
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await Employer.findOne({ email });
+        if (!user) {
+          return done(null, false, { message: "User not found" });
         }
+
+        const validate = await user.isValidPassword(password);
+
+        if (!validate) {
+          return done(null, false, { message: "Wrong Password" });
+        }
+
+        return done(null, user, { message: "Logged in Successfully" });
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+passport.use(
+  "admin-jwt",
+  new JWTstrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: (req) => {
+        let token = null;
+        if (req && req.cookies) {
+          token = req.cookies[process.env.COOKIE_KEY];
+        }
+        return token;
+      },
     },
     async (token, done) => {
-        try {
-            return done(null, token.user);
-        } catch (error) {
-            done(error);
-        }
+      try {
+        if (token.user.role === "Admin") return done(null, token.user);
+        else done(null, false);
+      } catch (error) {
+        done(error);
+      }
     }
-));
+  )
+);
+
+passport.use(
+  "employer-jwt",
+  new JWTstrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: (req) => {
+        let token = null;
+        if (req && req.cookies) {
+          token = req.cookies[process.env.COOKIE_KEY];
+        }
+        return token;
+      },
+    },
+    async (token, done) => {
+      try {
+        if (token.user.role === "Employer") return done(null, token.user);
+        else done(null, false);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
--- server/routes/employer.js
+++ server/routes/employer.js
@@ -1,9 +1,91 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Express');
+router.post("/signup", async (req, res, next) => {
+  passport.authenticate("employer-signup", async (err, user, info) => {
+    try {
+      if (err) {
+        return res.status(400).json(err);
+      }
+
+      if (!user) {
+        return res.status(400).json({ message: info.message });
+      }
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+
+        const token = jwt.sign({ user }, process.env.JWT_SECRET);
+
+        const cookieOptions = {
+          expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+          httpOnly: true,
+        };
+
+        return res
+          .cookie(process.env.COOKIE_KEY, token, cookieOptions)
+          .status(200)
+          .json({ user });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
+
+router.post("/login", async (req, res, next) => {
+  passport.authenticate("employer-login", async (err, user, info) => {
+    try {
+      if (err) {
+        const error = new Error("An error occurred.");
+
+        return next(error);
+      }
+
+      if (!user) {
+        return res.status(401).json({ message: info.message });
+      }
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+
+        const token = jwt.sign({ user }, process.env.JWT_SECRET);
+
+        const cookieOptions = {
+          expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+          httpOnly: true,
+        };
+
+        return res
+          .cookie(process.env.COOKIE_KEY, token, cookieOptions)
+          .status(200)
+          .json({ user });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
+
+router.use(passport.authenticate("employer-jwt", { session: false }));
+
+router.get("/auth", (req, res) => {
+  res.json({ user: req.user });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res
+    .status(200)
+    .clearCookie("token")
+    .json({ status: true, message: "Successfully logged out" });
+});
+
+router.get("/", (req, res) => {
+  res.send("Express");
 });
 
 module.exports = router;
