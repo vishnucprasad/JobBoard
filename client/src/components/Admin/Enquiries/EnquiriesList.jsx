a998991d120e8b693959d4f3743477d3bcf2c088
Vishnu C Prasad <connect.vishnucp@gmail.com>
2021-09-08 11:34:49 +0530

    Modified enquiries page design in Admin panel

--- client/src/components/Admin/Enquiries/EnquiriesList.jsx
+++ client/src/components/Admin/Enquiries/EnquiriesList.jsx
@@ -1,118 +1,65 @@
 import React from "react";
-import { DataGrid } from "@material-ui/data-grid";
 import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import MailIcon from "@material-ui/icons/Mail";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import UserIcon from "../../../images/user-icon.png";
 
 const EnquiriesList = () => {
-  const columns = [
-    { field: "id", headerName: "ID", headerClassName: "bg-light" },
+  const enquiries = [
     {
-      field: "name",
-      headerName: "Name",
-      headerClassName: "bg-light",
-      flex: 1,
-      minWidth: 150,
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      headerClassName: "bg-light",
-      flex: 1,
-      minWidth: 150,
-    },
-    {
-      field: "subject",
-      headerName: "Subject",
-      headerClassName: "bg-light",
-      flex: 1,
-      minWidth: 150,
-    },
-    {
-      field: "message",
-      headerName: "Message",
-      headerClassName: "bg-light",
-      flex: 1,
-      minWidth: 150,
-    },
-    {
-      field: "enquiryId",
-      headerName: "View",
-      headerClassName: "bg-light",
-      flex: 1,
-      minWidth: 150,
-      renderCell: (params) => (
-        <Link
-          to={`/admin/enquiries/view/${params.value}`}
-          className="btn btn-primary btn-sm btn-block font-weight-bold p-0"
-        >
-          View
-        </Link>
-      ),
-    },
-  ];
-
-  const rows = [
-    {
-      id: 1,
-      name: "Vishnu",
-      email: "mail@vishnucprasad.in",
-      subject: "Lorem ipsum",
+      id: "dgkjsvfbliuhblfuglshf1",
+      subject: "Lorem Ipsum",
       message:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam harum quis in nam aliquid error, voluptas repellendus porro. Repudiandae assumenda atque velit dolorem amet mollitia temporibus quidem aperiam est magnam.",
-      enquiryId: "dgkjsvfbliuhblfuglshf",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolore delectus aliquam quis ducimus ipsa dicta sint vero doloremque quos, laborum, similique quisquam ab vel recusandae architecto tenetur possimus sed.",
+      date: "01-01-0001",
+      userDetails: {
+        id: "dgkjsvfbliuhblfuglsdddddhf1",
+        name: "Vishnu",
+        email: "mail@vishnucprasad.in",
+        mobile: "+918157983670",
+      },
     },
     {
-      id: 2,
-      name: "Vishnu",
-      email: "mail@vishnucprasad.in",
-      subject: "Lorem ipsum",
+      id: "dgkjsvfbliuhblfuglshf2",
+      subject: "Lorem Ipsum",
       message:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam harum quis in nam aliquid error, voluptas repellendus porro. Repudiandae assumenda atque velit dolorem amet mollitia temporibus quidem aperiam est magnam.",
-      enquiryId: "dgkjsvfbliuhblfuglshf",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolore delectus aliquam quis ducimus ipsa dicta sint vero doloremque quos, laborum, similique quisquam ab vel recusandae architecto tenetur possimus sed.",
+      date: "01-01-0001",
+      userDetails: {
+        id: "dgkjsvfbliuhblfuglsdddddhf2",
+        name: "Vishnu",
+        email: "mail@vishnucprasad.in",
+        mobile: "+918157983670",
+        displayPicture:
+          "https://lh3.googleusercontent.com/a-/AOh14GhBd0XUn7qW2xHxlqZK-Fr7Pp8D8IVJnMHHa5VT=s96-c",
+      },
     },
     {
-      id: 3,
-      name: "Vishnu",
-      email: "mail@vishnucprasad.in",
-      subject: "Lorem ipsum",
+      id: "dgkjsvfbliuhblfuglshf3",
+      subject: "Lorem Ipsum",
       message:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam harum quis in nam aliquid error, voluptas repellendus porro. Repudiandae assumenda atque velit dolorem amet mollitia temporibus quidem aperiam est magnam.",
-      enquiryId: "dgkjsvfbliuhblfuglshf",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolore delectus aliquam quis ducimus ipsa dicta sint vero doloremque quos, laborum, similique quisquam ab vel recusandae architecto tenetur possimus sed.",
+      date: "01-01-0001",
+      userDetails: {
+        id: "dgkjsvfbliuhblfuglsdddddhf3",
+        name: "Vishnu",
+        email: "mail@vishnucprasad.in",
+        mobile: "+918157983670",
+      },
     },
     {
-      id: 4,
-      name: "Vishnu",
-      email: "mail@vishnucprasad.in",
-      subject: "Lorem ipsum",
+      id: "dgkjsvfbliuhblfuglshf4",
+      subject: "Lorem Ipsum",
       message:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam harum quis in nam aliquid error, voluptas repellendus porro. Repudiandae assumenda atque velit dolorem amet mollitia temporibus quidem aperiam est magnam.",
-      enquiryId: "dgkjsvfbliuhblfuglshf",
-    },
-    {
-      id: 5,
-      name: "Vishnu",
-      email: "mail@vishnucprasad.in",
-      subject: "Lorem ipsum",
-      message:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam harum quis in nam aliquid error, voluptas repellendus porro. Repudiandae assumenda atque velit dolorem amet mollitia temporibus quidem aperiam est magnam.",
-      enquiryId: "dgkjsvfbliuhblfuglshf",
-    },
-    {
-      id: 6,
-      name: "Vishnu",
-      email: "mail@vishnucprasad.in",
-      subject: "Lorem ipsum",
-      message:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam harum quis in nam aliquid error, voluptas repellendus porro. Repudiandae assumenda atque velit dolorem amet mollitia temporibus quidem aperiam est magnam.",
-      enquiryId: "dgkjsvfbliuhblfuglshf",
-    },
-    {
-      id: 7,
-      name: "Vishnu",
-      email: "mail@vishnucprasad.in",
-      subject: "Lorem ipsum",
-      message:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam harum quis in nam aliquid error, voluptas repellendus porro. Repudiandae assumenda atque velit dolorem amet mollitia temporibus quidem aperiam est magnam.",
-      enquiryId: "dgkjsvfbliuhblfuglshf",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolore delectus aliquam quis ducimus ipsa dicta sint vero doloremque quos, laborum, similique quisquam ab vel recusandae architecto tenetur possimus sed.",
+      date: "01-01-0001",
+      userDetails: {
+        id: "dgkjsvfbliuhblfuglsdddddhf4",
+        name: "Vishnu",
+        email: "mail@vishnucprasad.in",
+        mobile: "+918157983670",
+      },
     },
   ];
 
@@ -122,11 +69,78 @@ const EnquiriesList = () => {
         <div className="card-header pb-0">
           <div className="text-uppercase d-md-flex mb-0">
             <div className="font-weight-bolder h6">Enquiries</div>
+            <div className="w-md-25 ml-auto">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text text-twitter">
+                    <SearchIcon />
+                  </span>
+                </div>
+                <input
+                  className="form-control"
+                  id="exampleInputIcon1"
+                  placeholder="Search for jobs..."
+                  type="text"
+                  aria-label="Input with icon left"
+                />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="card-body shadow-inset rounded m-3">
-          <div style={{ height: 500, width: "100%" }}>
-            <DataGrid rows={rows} columns={columns} pageSize={10} />
+        <div className="card-body shadow-inset rounded m-3 px-3 pt-3 pb-0">
+          <div className="scroll-70 px-2 pt-2 pb-0">
+            {enquiries.map((enquiry) => (
+              <div key={enquiry.id} className="card shadow-soft rounded mb-4">
+                <div className="card-body">
+                  <div className="row align-items-center">
+                    <div className="col-md-8">
+                      <Link to={`/admin/enquiries/view/${enquiry.id}`}>
+                        <div className="d-flex align-items-center">
+                          <img
+                            src={
+                              enquiry.userDetails.displayPicture
+                                ? enquiry.userDetails.displayPicture
+                                : UserIcon
+                            }
+                            alt=""
+                            className="img-fluid rounded-circle user-image-sm mr-3"
+                          />
+                          <div>
+                            <h6 className="font-weight-bold m-0">
+                              {enquiry.userDetails.name}
+                            </h6>
+                            <div className="badge badge-twitter font-weight-bolder">
+                              {enquiry.userDetails.email}
+                            </div>
+                          </div>
+                        </div>
+                        <h4 className="font-weight-bold my-3">
+                          {enquiry.subject}
+                        </h4>
+                        <p className="font-weight-bold mt-3 mb-2">
+                          <div className="mr-3 text-truncate">
+                            <MailIcon className="text-twitter" />
+                            {enquiry.message}
+                          </div>
+                        </p>
+                      </Link>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="d-flex align-items-center">
+                        <div className="text-center w-100 mr-4">
+                          <p className="font-weight-bold m-0">{enquiry.date}</p>
+                        </div>
+                        <div className="w-100">
+                          <button className="btn btn-primary btn-sm btn-block text-danger text-uppercase font-weight-bold mt-3">
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
