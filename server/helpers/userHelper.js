e83a38482dcfdb5ff53c86f4ed60157af8a3afa1
Vishnu C Prasad <connect.vishnucp@gmail.com>
2021-10-13 09:08:32 +0530

    Setup route in server for getting data for user Homepage

--- server/helpers/userHelper.js
+++ server/helpers/userHelper.js
@@ -2,7 +2,41 @@ const mongoose = require("mongoose");
 const Job = require("../models/job");
 
 module.exports = {
-  getAllJobs: (employerId) => {
+  getRecentJobs: () => {
+    return new Promise((resolve, reject) => {
+      Job.aggregate([])
+        .lookup({
+          from: "employers",
+          localField: "employerId",
+          foreignField: "_id",
+          as: "employerDetails",
+        })
+        .unwind("employerDetails")
+        .project("-employerDetails.password")
+        .sort({ createdAt: 1 })
+        .limit(4)
+        .then((jobs) => resolve(jobs))
+        .catch((error) => reject(error));
+    });
+  },
+  getHighestPayingJobs: () => {
+    return new Promise((resolve, reject) => {
+      Job.aggregate([])
+        .lookup({
+          from: "employers",
+          localField: "employerId",
+          foreignField: "_id",
+          as: "employerDetails",
+        })
+        .unwind("employerDetails")
+        .project("-employerDetails.password")
+        .sort({ salary: -1 })
+        .limit(4)
+        .then((jobs) => resolve(jobs))
+        .catch((error) => reject(error));
+    });
+  },
+  getAllJobs: () => {
     return new Promise((resolve, reject) => {
       Job.aggregate([])
         .lookup({
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,20 @@ const express = require("express");
 const userHelper = require("../helpers/userHelper");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  userHelper
+    .getRecentJobs()
+    .then((recentJobs) => {
+      userHelper
+        .getHighestPayingJobs()
+        .then((highestPayingJobs) =>
+          res.json({ recentJobs, highestPayingJobs })
+        )
+        .catch((error) => res.json(error));
+    })
+    .catch((error) => res.json(error));
+});
+
 router.get("/jobs", (req, res) => {
   userHelper
     .getAllJobs()
