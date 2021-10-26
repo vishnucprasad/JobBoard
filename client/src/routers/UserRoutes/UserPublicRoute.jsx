3b0ad2419acbb2f568d5b9a2f50417db026bfa46
Vishnu C Prasad <connect.vishnucp@gmail.com>
2021-10-26 11:35:44 +0530

    Modified User Routers

--- client/src/routers/UserRoutes/UserPublicRoute.jsx
+++ client/src/routers/UserRoutes/UserPublicRoute.jsx
@@ -1,39 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useAuthState } from "../../contexts/AuthStateProvider";
-import Axios from "../../axios/axios";
-import { authActionTypes } from "../../reducers/auth";
-import Loader from "../../pages/Loader";
 
 const UserPublicRoute = ({ component: Component, ...rest }) => {
-  const [{ auth }, dispatch] = useAuthState();
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    Axios.get("/auth")
-      .then(({ data: { user } }) => {
-        user &&
-          dispatch({
-            type: authActionTypes.LOGIN,
-            auth: user,
-          });
-        setIsLoading(false);
-      })
-      .catch(() => {
-        dispatch({
-          type: authActionTypes.LOGOUT,
-        });
-        setIsLoading(false);
-      });
-  }, [dispatch]);
+  const [{ auth }] = useAuthState();
 
   const isAuthenticated = !!auth._id && auth.role === "Jobseeker";
 
-  return isLoading ? (
-    <div>
-      <Loader />
-    </div>
-  ) : (
+  return (
     <Route
       {...rest}
       component={(props) =>
--- client/src/routers/UserRoutes/UserRouter.jsx
+++ client/src/routers/UserRoutes/UserRouter.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import NotFound from "../../pages/NotFound";
 import FindJob from "../../pages/User/FindJob";
@@ -6,9 +7,36 @@ import UserSignUp from "../../pages/User/UserSignUp";
 import UserPublicRoute from "./UserPublicRoute";
 import UserLogin from "../../pages/User/UserLogin";
 import ViewJob from "../../pages/User/ViewJob";
+import { useAuthState } from "../../contexts/AuthStateProvider";
+import { authActionTypes } from "../../reducers/auth";
+import Axios from "../../axios/axios";
+import Loader from "../../pages/Loader";
 
 const UserRouter = () => {
-  return (
+  const [, dispatch] = useAuthState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    Axios.get("/auth")
+      .then(({ data: { user } }) => {
+        user &&
+          dispatch({
+            type: authActionTypes.LOGIN,
+            auth: user,
+          });
+        setIsLoading(false);
+      })
+      .catch(() => {
+        dispatch({
+          type: authActionTypes.LOGOUT,
+        });
+        setIsLoading(false);
+      });
+  }, [dispatch]);
+
+  return isLoading ? (
+    <Loader />
+  ) : (
     <Switch>
       <Route exact path="/user" component={HomePage} />
       <Route path="/user/find" component={FindJob} />
