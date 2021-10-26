1519e91b7438d8912e3b4c9e175616bbe428e6a7
Vishnu C Prasad <connect.vishnucp@gmail.com>
2021-10-26 11:20:01 +0530

    Setup user login and logout route in server

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -161,6 +161,34 @@ passport.use(
 );
 
 passport.use(
+  "user-login",
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return done(null, false, { message: "User not found" });
+        }
+
+        const validate = await user.isValidPassword(password);
+
+        if (!validate) {
+          return done(null, false, { message: "Wrong Password" });
+        }
+
+        return done(null, user, { message: "Logged in Successfully" });
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+passport.use(
   "admin-jwt",
   new JWTstrategy(
     {
--- server/routes/user.js
+++ server/routes/user.js
@@ -46,6 +46,40 @@ router.post("/signup", async (req, res, next) => {
   })(req, res, next);
 });
 
+router.post("/login", async (req, res, next) => {
+  passport.authenticate("user-login", async (err, user, info) => {
+    try {
+      if (err) {
+        const error = new Error("An error occurred.");
+
+        return next(error);
+      }
+
+      if (!user) {
+        return res.status(401).json({ message: info.message });
+      }
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+
+        const token = jwt.sign({ user }, process.env.JWT_SECRET);
+
+        const cookieOptions = {
+          expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+          httpOnly: true,
+        };
+
+        return res
+          .cookie(process.env.COOKIE_KEY, token, cookieOptions)
+          .status(200)
+          .json({ user });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
+
 router.get("/", (req, res) => {
   userHelper
     .getRecentJobs()
@@ -85,6 +119,14 @@ router.use(passport.authenticate("user-jwt", { session: false }));
 
 router.get("/auth", (req, res) => {
   res.json({ user: req.user });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res
+    .status(200)
+    .clearCookie("token")
+    .json({ status: true, message: "Successfully logged out" });
 });
 
 module.exports = router;
