213ff1078c1821d043d70e433f8c26d27d16a228
Vishnu C Prasad <connect.vishnucp@gmail.com>
2021-11-17 11:30:47 +0530

    Modified Contact us page

--- client/src/components/User/ContactUs/ContactForm.jsx
+++ client/src/components/User/ContactUs/ContactForm.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import PersonIcon from "@material-ui/icons/Person";
+import SubjectIcon from "@material-ui/icons/Subject";
 import MailIcon from "@material-ui/icons/Mail";
 
 const ContactForm = () => {
@@ -22,6 +23,7 @@ const ContactForm = () => {
               placeholder="Name"
               type="text"
               aria-label="contact name input"
+              required
             />
           </div>
         </div>
@@ -41,6 +43,27 @@ const ContactForm = () => {
               placeholder="Email"
               type="email"
               aria-label="contact email input"
+              required
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label for="nameInputIcon2">Your Subject</label>
+          <div className="input-group mb-4">
+            <div className="input-group-prepend">
+              <span className="input-group-text">
+                <span className="text-twitter">
+                  <SubjectIcon />
+                </span>
+              </span>
+            </div>
+            <input
+              className="form-control"
+              id="nameInputIcon2"
+              placeholder="Subject"
+              type="text"
+              aria-label="contact name input"
+              required
             />
           </div>
         </div>
@@ -51,14 +74,15 @@ const ContactForm = () => {
             placeholder="Enter your message..."
             id="exampleFormControlTextarea2"
             rows="4"
+            required
           ></textarea>
         </div>
+        <div className="text-center px-0 mx-auto">
+          <button type="submit" className="btn btn-primary">
+            Send message
+          </button>
+        </div>
       </form>
-      <div className="text-center px-0 mx-auto">
-        <button type="submit" className="btn btn-primary">
-          Send message
-        </button>
-      </div>
     </div>
   );
 };
